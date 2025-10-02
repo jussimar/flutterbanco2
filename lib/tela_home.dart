b5c49f64f3d627f2f59162afd40f3b483272e0c9
jussimar <jussimar@jussimarleal.com.br>
2025-10-02 15:06:29 -0300

    repositirio para crud

---
+++ lib/tela_home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:mimpedir/tela_cad_restaurante.dart';
+import 'package:mimpedir/tela_edit_restaurante.dart';
+
+class TelaHome extends StatelessWidget{
+  TelaHome({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tela Home')),
+      body: Padding(padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Lista de Restaurantes: "),
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => TelaCadRestaurante()));
+            }, child: Text("Cadastrar Restaurantes")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:
+              [
+                Column(
+                  children: [
+                    Text("Meu restaurante"),
+                    Text("Comida Mexicana")
+                  ],
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){}, child: Text("Excluir")),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> TelaEditRestaurante()));
+                }, child: Text("Editar")),
+              ],
+            ),
+          ],
+      ),
+      )
+    );
+  }
+}
---
+++ lib/usuario.dart
@@ -0,0 +1,34 @@
+class Usuario{
+  int? _codigo;
+  String? _nome;
+  String? _login;
+  String? _senha;
+
+  Usuario({String? nome, int? codigo, String? senha, String? login}){
+    _senha = senha;
+    _codigo = codigo;
+    _nome = nome;
+    _login = login;
+  }
+
+  //getters
+  //escrita do get tradicional
+  String? get login{
+    return _login;
+  }
+  //escrita do get no estilo dart
+  String? get nome => _nome;
+  String? get senha => _senha;
+  int? get codigo => _codigo;
+
+  //setters
+  //escrita do set tradicional
+  set login(String? login){
+    _login = login;
+  }
+
+  //escrita do set no estilo dart
+  set nome(String? nome) => _nome = nome;
+  set senha(String? senha) => _senha = senha;
+  set codigo(int? codigo) => _codigo = codigo;
+}
